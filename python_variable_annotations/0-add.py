cab2b1edc6cc05b7210139a0ae546f1e5530808e
root <root@ip-10-42-84-200.ec2.internal>
2026-04-06 22:03:12 +0000

    Add documented type-annotated function add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
+"""
+Module 0-add
+Provides a type-annotated function to add two floats.
+"""
+
 def add(a: float, b: float) -> float:
+    """
+    Add two floating point numbers.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
     return a + b
