c6c230f86aab24cf0c55548283dd595e6cbb3459
root <root@ip-10-42-135-148.ec2.internal>
2026-04-20 21:21:14 +0000

    Add function schools_by_topic to return schools with a specific topic

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Find schools by topic
+"""
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of schools having a specific topic.
+    - mongo_collection: pymongo collection object
+    - topic: string, topic searched
+    """
+    return list(mongo_collection.find({ "topics": topic }))
