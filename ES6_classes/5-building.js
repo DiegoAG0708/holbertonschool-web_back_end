77d870e835d3855742f7489b5034879bdc0fd112
root <root@ip-10-42-244-54.ec2.internal>
2026-04-27 22:11:43 +0000

    Implement Building abstract class with sqft and evacuationWarningMessage enforcement

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+// 5-building.js
+
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = sqft;
+
+    // Enforce abstract method requirement
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  // Getter
+  get sqft() {
+    return this._sqft;
+  }
+}
